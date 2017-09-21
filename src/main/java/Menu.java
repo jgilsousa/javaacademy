2eff5ee4e266031fef09b83a33dc83a5692afec9
jgilsousa <j.gil.d.sousa@gmail.com>
2017-09-21 21:08:55 +0100

    first commit

---
+++ src/main/java/Menu.java
@@ -0,0 +1,16 @@
+package presentation;
+
+
+public class Menu {
+
+public static void display(){
+
+    System.out.println();
+
+
+
+}
+
+
+
+}
---
+++ src/main/java/domain/App.java
@@ -0,0 +1,30 @@
+package domain;
+
+import com.sun.org.apache.xpath.internal.SourceTree;
+
+public class App {
+
+	public static void main(String[] args) {
+
+
+		//esta classe é para correr com Junit e deverá testar uma classe 'ClientService'
+		
+		//Fluxo normal da App => App com o metodo main, instancia ClientService
+		//ClientService usa Persistence e esta classe simula persistencia com listas de Client em memória
+		
+		//Funcionalidades a implementar: guardar n Client e listá-los
+		//ao guardar um Client gerar um identificador interno 
+		//ao guardar um Client, associar uma corda à ordem e um cartão débito
+
+		Menu.dispaly();
+
+
+
+
+
+
+
+
+	}
+
+}
